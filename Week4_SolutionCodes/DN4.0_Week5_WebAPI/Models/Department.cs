33e39d1ecf8137027ba63a298c5eaf90749e4e1d
Abhranil Dasgupta <abhranilnxt@gmail.com>
2025-07-13 16:30:12 +0530

    Delete Week4_SolutionCodes/DN4.0_Week5_WebAPI directory

--- Week4_SolutionCodes/DN4.0_Week5_WebAPI/Models/Department.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DN4._0_Week5_WebAPI.Models
-{
-    public class Department
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- Week4_SolutionCodes/DN4.0_Week5_WebAPI/Models/Skill.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DN4._0_Week5_WebAPI.Models
-{
-    public class Skill
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
